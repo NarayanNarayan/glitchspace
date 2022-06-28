3f9a3d76652e63352a22675e6fbbe6cd05a5b2be
NarayanNarayan <narayan1011g@gmail.com>
2022-06-29 02:00:48 +0530

    updated

--- lib/filedegenerator.cpp
+++ lib/filedegenerator.cpp
@@ -18,12 +18,53 @@ vector<int> permutationvec(int n){
     }
     return rv;
 }
-void test1(){
-    vector<int> a=permutationvec(10);
-    for(auto i:a) cout<<i<<" "; cout<<endl;
+vector<int> inv(vector<int> v){
+    int n=v.size();
+    vector<int> v_inv(n);
+    for(int i=0;i<n;i++) v_inv[v[i]]=i;
+    /**
+     for(int i=0;i<n;i++) v[v_inv[i]]=i; can also be used
+     same as above 
+     v_inv[v[v_inv[i]]]=v_inv[i]
+     replace v_inv[i]=j
+     v_inv[v[j]]=j
+     */
+    return v_inv;
+}
+char* encrypt(char* inp,vector<int> m){
+    int n=m.size();
+    char* out0=(char*)malloc(n);
+    char* out1=(char*)malloc(n);
+    for(int i=0;i<n;i++) out0[m[i]]=inp[i];
+    for(int i=0;i<n;i++) out1[i]=inp[m[i]];
+
+    return out0;
+    //out2[minv[i]]=out[i]
+    //for(int i=0;i<n;i++) m_inv[m[i]]=i;
+
+}
+char* encrypt2(char* inp,vector<int> m){
+    int n=m.size();
+    char* out0=(char*)malloc(n);
+    char* out1=(char*)malloc(n);
+    for(int i=0;i<n;i++) out0[m[i]]=inp[i];
+    for(int i=0;i<n;i++) out1[i]=inp[m[i]];
+    return out1;
+}
+void testv(vector<int> v){
+    for(auto i:v) cout<<i<<" ";cout<<endl;
+}
+void test1(char* buff){
+    vector<int> m=permutationvec(25);
+    testv(m);
+    cout<<encrypt(buff,m)<<endl;
+    cout<<encrypt2(buff,m)<<endl;
+    cout<<encrypt(encrypt(buff,m),inv(m))<<endl;
+    cout<<encrypt2(encrypt2(buff,m),inv(m))<<endl;
 }
 int main(){
     srand((unsigned)time(0));
-    for(int i=0;i<30;i++) test1();
+    char buffer[]="Hellow World Hellow World\n";
+    for(int i=0;i<1;i++) test1(buffer);
     return 0;
 }
